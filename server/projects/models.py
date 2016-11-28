bf456d383fb5832750c272d2d24b181c694fa7ca
SergeGaliullin <sergei.galiullin@gmail.com>
2016-11-28 10:48:24 -0200

    Added project model; updated  req with Pillow

--- server/projects/models.py
+++ server/projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):    
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    duration = models.CharField(max_length=20)
+    avatar = models.ImageField(upload_to='media')
+
+    def __str__(self):
+        return self.name
