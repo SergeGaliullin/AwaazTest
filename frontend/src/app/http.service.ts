81c191e9860131ee04cfb0b6f095bc3e73852abf
SergeGaliullin <sergei.galiullin@gmail.com>
2016-11-28 13:17:49 -0200

    Added http service

---
+++ frontend/src/app/http.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/Rx';
+
+@Injectable()
+export class HttpService {
+  projects_url: string = 'http://localhost:8000/api/projects/';
+ 
+
+  constructor(private http: Http) {  }
+
+  getProjectList(){
+    const headers = new Headers();  
+    headers.append("Accept", "application/json"); 
+
+    return this.http.get(this.projects_url, {headers: headers})
+    .map((response: Response ) => response.json() );
+  }
+
+  getPost(id: string) {    
+    return this.http.get(this.projects_url + id)
+    .map((response: any ) => response.json()) ;        
+  }
+}
