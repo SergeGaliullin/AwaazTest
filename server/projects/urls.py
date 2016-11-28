6f2d7362fe4cbbfd495cdde9c5d3521c10013715
SergeGaliullin <sergei.galiullin@gmail.com>
2016-11-28 11:39:11 -0200

    Fixed urls and views

--- server/projects/urls.py
+++ server/projects/urls.py
@@ -3,9 +3,12 @@ from rest_framework.routers import DefaultRouter
 
 from .views import ProjectsViewSet, TaskViewSet
 
-urlpatterns = []
+urlpatterns = [
+    
+]
 
 router = DefaultRouter()
 router.register(r'projects', ProjectsViewSet, base_name='ProjectsViewSet')
+router.register(r'tasks', TaskViewSet, base_name='TaskViewSet')
 
 urlpatterns += router.urls
--- server/projects/views.py
+++ server/projects/views.py
@@ -3,10 +3,10 @@ from rest_framework import viewsets
 from .serializers import ProjectSerializer, TaskSerializer
 from .models import Project, Task
 
-class ProjectsViewSet(viewsets.GenericViewSet):
+class ProjectsViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
 
-class TaskViewSet(viewsets.GenericViewSet):
+class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
