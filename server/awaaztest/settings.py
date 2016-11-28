f5a31d739f62a549aa6f2a4942e94b93be6bfa08
SergeGaliullin <sergei.galiullin@gmail.com>
2016-11-28 11:49:55 -0200

    gitignore got migrations files; settings static

--- server/awaaztest/settings.py
+++ server/awaaztest/settings.py
@@ -122,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_deployed')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/user_images')
+MEDIA_URL = '/static/user_images/'
--- server/projects/models.py
+++ server/projects/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     duration = models.CharField(max_length=20)
-    avatar = models.ImageField(upload_to='media')
+    avatar = models.ImageField(upload_to='project_images')
 
     def __str__(self):
         return self.name
