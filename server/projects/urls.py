68e0241f825142344658f9a23a350e1a01e4057a
SergeGaliullin <sergei.galiullin@gmail.com>
2016-11-28 12:37:29 -0200

    Updated views to show tasks related to project

--- server/projects/urls.py
+++ server/projects/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
-from .views import ProjectsViewSet, TaskViewSet
+from .views import ProjectsViewSet, TaskList, TaskViewSet
 
 urlpatterns = [
     
@@ -10,5 +10,7 @@ urlpatterns = [
 router = DefaultRouter()
 router.register(r'projects', ProjectsViewSet, base_name='ProjectsViewSet')
 router.register(r'tasks', TaskViewSet, base_name='TaskViewSet')
+router.register(r'project/(?P<project_id>\d+)/tasks', TaskList, base_name='TaskList')
+
 
 urlpatterns += router.urls
--- server/projects/views.py
+++ server/projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import mixins
 from .serializers import ProjectSerializer, TaskSerializer
 from .models import Project, Task
 
@@ -7,6 +8,14 @@ class ProjectsViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
 
-class TaskViewSet(viewsets.ModelViewSet):
+class TaskViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin,
+                     mixins.UpdateModelMixin, viewsets.GenericViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+class TaskList(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        id = self.kwargs['project_id']
+        return Project.objects.get(pk=id).task_set.all()
